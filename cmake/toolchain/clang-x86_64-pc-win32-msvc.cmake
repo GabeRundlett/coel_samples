7388d5bd9fee6d90e6556e43df3de27195813a90
GabeRundlett <gabe@rundlett.com>
2022-02-23 10:43:01 -0700

    Use Ninja Multi-Config

--- cmake/toolchain/clang-x86_64-pc-win32-msvc.cmake
+++ cmake/toolchain/clang-x86_64-pc-win32-msvc.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang)
 
-set(triple ${CMAKE_SYSTEM_PROCESSOR}-windows-clang)
+set(triple x86_64-pc-win32-msvc)
 include(toolchain/utils/common)
 include(toolchain/utils/common-clang)
 include(toolchain/utils/vcpkg)
